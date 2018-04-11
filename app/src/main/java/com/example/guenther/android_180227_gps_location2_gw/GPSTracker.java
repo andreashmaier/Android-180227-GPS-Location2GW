687b620eb046f18f8ef0c4863042ac10fe24fc52
wagenhuber <wagenhuber@gmx.net>
2018-04-11 18:25:49 +0200

    Workshop-Version (Code stellenweise entfernt)

--- app/src/main/java/com/example/guenther/android_180227_gps_location2_gw/GPSTracker.java
+++ app/src/main/java/com/example/guenther/android_180227_gps_location2_gw/GPSTracker.java
@@ -10,9 +10,9 @@ import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.widget.Toast;
 
-/**
- * Created by guenther on 27.02.18.
- */
+
+
+
 
 public class GPSTracker implements LocationListener {
 
@@ -26,27 +26,13 @@ public class GPSTracker implements LocationListener {
     public Location getLocation() {
 
         //Schritt 1 - Berechtigungen überprüfen (Determine whether you have been granted a particular permission)
-        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(context, "Berechtigung nicht erteilt", Toast.LENGTH_SHORT).show();
-        }
 
 
         //Schritt 2 - LocationManager erstellen
-        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
 
 
         //Schritt 3 - Prüfen ob GPS am Geräte aktiviert und Location Object erstellen
-        boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
 
-        if (isGPSEnabled) {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 6000, 10, this);
-            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            return location;
-
-        } else {
-            Toast.makeText(context, "Bitte GPS aktivieren", Toast.LENGTH_LONG).show();
-        }
-        return null;
     }
 
     @Override
--- app/src/main/java/com/example/guenther/android_180227_gps_location2_gw/MainActivity.java
+++ app/src/main/java/com/example/guenther/android_180227_gps_location2_gw/MainActivity.java
@@ -20,45 +20,17 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState); //extending the existing parent class' method
         setContentView(R.layout.activity_main);
         btnGetLoc = (Button) findViewById(R.id.btnGetLoc);
         textView = findViewById(R.id.textView);
 
 
-
         //Schritt 4 - Anfordern Berechtigungen (Requests permissions to be granted to this application)
-        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 4711);
 
-
+        
         //Schritt 5 - onClickListener für Button
-        btnGetLoc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                GPSTracker gpsTracker = new GPSTracker(getApplicationContext());
-                Location location = gpsTracker.getLocation();
 
-                if (location != null) {
-                    double latitude = location.getLatitude(); // Get the latitude, in degrees.
-                    double longitude = location.getLongitude(); // Get the longitude, in degrees.
-                    double altitude = location.getAltitude(); // Get the altitude if available, in meters above the WGS 84 reference ellipsoid.
-                    double timestamp = location.getTime(); // Return the UTC time of this fix, in milliseconds since January 1, 1970.
-                    float speed = location.getSpeed(); // Get the speed if it is available, in meters/second over ground.
-                    float bearing = location.getBearing(); // Get the bearing (Peilung nach Norden), in degrees.
-
-                    textView.setText("Breitengrad: " + latitude +
-                            "\nLängengrad: " + longitude +
-                            "\nHöhe: " + altitude +
-                            "\nZeitstempel unix:" + timestamp +
-                            "\nSpeed in m/s: " + speed +
-                            "\nPeilung in Grad: " + bearing
-                    );
-                    //Toast.makeText(getApplicationContext(), "Breitengrad: " + latitude + "\n Längengrad: " + longitude, Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(getApplicationContext(), "location ist null!!", Toast.LENGTH_LONG);
-                }
-            }
-        });
     }
 
     public static void onLocationChanged() {
@@ -66,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public static void onProviderDisabled() {
-        Log.i(TAG, "test Provider diabled");
+        Log.i(TAG, "test Provider disabled");
     }
 
     public static void onProviderEnabled() {
